f2d3b8092ffc5dbee7207df50484c8974ea7c459
Craig Richards <craig@geekcomputers.co.uk>
2012-06-11 19:43:59 +0100

    Used to get a command shell when you do not have access to the Run dialog box in windows

---
+++ cmd.bat
@@ -0,0 +1,14 @@
+@echo off
+
+REM The above command turns off the output for the script
+
+REM Script Name		: cmd.bat
+REM Author				: Craig Richards
+REM Created			: 03-Novemeber-2011
+REM Last Modified	: 
+REM Version				: 1.0
+REM Modifications		: 
+REM
+REM	 Description		: Used to get a command shell when you don't have access to the Run dialog box in windows
+
+Start
