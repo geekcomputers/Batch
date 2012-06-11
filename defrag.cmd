42017cb569e787715df81b9499aefa036dddfdee
Craig Richards <craig@geekcomputers.co.uk>
2012-06-11 19:42:50 +0100

    The batch file I used to use to schedule a defrag of the c:\, using the AT command

---
+++ defrag.cmd
@@ -0,0 +1,14 @@
+@echo off
+
+REM The above command turns off the output for the script
+
+REM Script Name		: defrag.cmd
+REM Author				: Craig Richards
+REM Created			: 24th-January-2008
+REM Last Modified	: 
+REM Version				: 1.0
+REM Modifications		: 
+REM
+REM	 Description		: The batch file I used to use to schedule a defrag of the c:\, using the AT command
+
+defrag c: >> c:\defrag.log
