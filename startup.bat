7824a0ae776d8f6616ed5016f3dc7128209e9caa
Sercan Sebetçi <86542016+sebetci@users.noreply.github.com>
2022-05-27 03:46:58 +0300

    add: help menu and documentation

--- startup.bat
+++ startup.bat
@@ -1,34 +1,55 @@
-@echo off
+:: ===========================================================================================================
+:: @file        startup.bat
+:: @brief       Custom start-up batch file for my current work PC
+:: @author      Craig Richards
+:: @date        08.12.2010
+:: @version     1.0
+:: @usage       startup.bat
+:: @see         https://github.com/sebetci/geekcomputers/Batch/startup.bat
+:: @reference   https://ss64.com/nt/subst.html
+:: @requirement https://www.thegeekstuff.com/scripts/puttycm.zip
+:: @changelog   1.1. Added the like to create a "virtual" drive for the putty keys
+:: @note        This script must be run in a command window with administrator rights.
+:: @test        OK
+:: @todo        The script should check if the requirements exist.
+:: @todo        The existence of files and directories should be checked.
+:: ===========================================================================================================
 
-REM The above command turns off the output for the script
+@ECHO OFF
 
-REM Script Name		: start-up.bat
-REM Author				: Craig Richards
-REM Created			: 08th-December-2010
-REM Last Modified	: 14th-May-2012
-REM Version				: 1.1
-REM Modifications		: 1.1 - 14th-May-2012
-REM							: 1.1 - Added the like to create a "virtual" drive for the putty keys
-REM
-REM	 Description		: Custom start-up batch file for my current work PC
+CALL :HELP
 
-echo %DATE% %TIME% >> u:\login.txt
+IF %ERRORLEVEL% EQU 0 (
+    ECHO %DATE% %TIME% >> C:\Login.txt
 
-REM Firewall Authentication
-REM =======================
+    :: Firewall Authentication
+    TELNET 192.168.46.1 259
+    TELNET 192.168.35.1 259
 
-telnet 192.168.46.1 259
-telnet 192.168.35.1 259
+    :: Load PuTTYCM
+    START PUTTYCM.exe
+    START /MIN NOTEPAD++.exe C:\Today.txt
+    START /MIN CMD.exe
 
-REM Load PuTTYCM
-REM ============
+    :: Create a "Virtual Drive"
+    :: Associate a path with a drive letter
+    SUBST K: "%%\Tulletss\Keys"
+)
 
-start puttycm.exe
+GOTO :EOF
 
-start /min notepad++ today.txt
-
-start /min cmd.exe
-
-REM Create a "Virtual Drive" - 1.2
-
-subst k: "%dropbox%\Tulletts\Keys"
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:: @function   This function prints the help menu on the screen.
+:: @parameter  None
+:: @return     Returns 1 if the help menu is displayed.
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:HELP
+    FOR %%H IN (/h /help -help -h) DO (
+        IF /I "%~1" EQU "%%~H" (
+            ECHO.
+            ECHO [BRIEF] Custom start-up batch file for my current work PC
+            ECHO [USAGE] startup.bat
+            EXIT /B 1
+        )
+    )
+    EXIT /B 0
