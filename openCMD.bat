c6093e5983c53f44f3b9663cb281fb7276f51e38
Sercan Sebetçi <86542016+sebetci@users.noreply.github.com>
2022-05-27 03:45:54 +0300

    add: help menu and documentation

--- openCMD.bat
+++ openCMD.bat
@@ -1,14 +1,36 @@
-@echo off
+:: ===========================================================================================================
+:: @file        openCMD.bat
+:: @brief       Used to get a command shell when you don't have access to the Run dialog box in windows
+:: @author      Craig Richards
+:: @date        03.11.2011
+:: @version     1.0
+:: @usage       openCMD.bat
+:: @see         https://github.com/sebetci/geekcomputers/Batch/openCMD.bat
+:: @reference   https://ss64.com/nt/start.html
+:: @test        OK
+:: ===========================================================================================================
 
-REM The above command turns off the output for the script
+@ECHO OFF
 
-REM Script Name		: cmd.bat
-REM Author				: Craig Richards
-REM Created			: 03-Novemeber-2011
-REM Last Modified	: 
-REM Version				: 1.0
-REM Modifications		: 
-REM
-REM	 Description		: Used to get a command shell when you don't have access to the Run dialog box in windows
+IF %ERRORLEVEL% EQU 0 (
+    :: Start a program, command or batch script, opens in a new window.
+    START
+)
 
-Start
+GOTO :EOF
+
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:: @function   This function prints the help menu on the screen.
+:: @parameter  None
+:: @return     Returns 1 if the help menu is displayed.
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:HELP
+    FOR %%H IN (/h /help -help -h) DO (
+        IF /I "%~1" EQU "%%~H" (
+            ECHO.
+            ECHO [BRIEF] Used to get a command shell when you don't have access to the Run dialog box in windows
+            ECHO [USAGE] openCMD.bat
+            EXIT /B 1
+        )
+    )
+    EXIT /B 0
