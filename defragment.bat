7fa9c196e87467f82f9ca7cd9ba55268214a2788
Sercan Sebetçi <86542016+sebetci@users.noreply.github.com>
2022-05-27 03:42:52 +0300

    add: help menu and documentation

--- defragment.bat
+++ defragment.bat
@@ -1,14 +1,42 @@
-@echo off
+:: ===========================================================================================================
+:: @file        defragment.bat
+:: @brief       The batch file I used to use to schedule a defrag of the c:\, using the AT command
+:: @author      Craig Richards
+:: @date        24.01.2008
+:: @version     1.0
+:: @usage       defragment.bat
+:: @see         https://github.com/sebetci/geekcomputers/Batch/defragment.bat
+:: @reference   https://ss64.com/nt/defrag.html
+:: @note        This script must be run in a command window with administrator rights.
+:: @test        OK
+:: @todo        Log file must be passed as a parameter from the command line.
+:: @todo        The existence of drivers should be checked.
+:: ===========================================================================================================
 
-REM The above command turns off the output for the script
+@ECHO OFF
 
-REM Script Name		: defrag.cmd
-REM Author				: Craig Richards
-REM Created			: 24th-January-2008
-REM Last Modified	: 
-REM Version				: 1.0
-REM Modifications		: 
-REM
-REM	 Description		: The batch file I used to use to schedule a defrag of the c:\, using the AT command
+:: The HELP function is called.
+CALL :HELP
 
-defrag c: >> c:\defrag.log
+IF %ERRORLEVEL% EQU 0 (
+    :: Defragment hard drive volumes.
+    DEFRAG C: >> C:\Defragment.log
+)
+
+GOTO :EOF
+
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:: @function   This function prints the help menu on the screen.
+:: @parameter  None
+:: @return     Returns 1 if the help menu is displayed.
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:HELP
+    FOR %%H IN (/h /help -help -h) DO (
+        IF /I "%~1" EQU "%%~H" (
+            ECHO.
+            ECHO [BRIEF] The batch file I used to use to schedule a defrag of the c:\, using the AT command
+            ECHO [USAGE] defragment.bat
+            EXIT /B 1
+        )
+    )
+    EXIT /B 0
